e86be7126b6edbc4f56d87f09582397947f4a209
weiwei20060505 <weiwei20060505@gmail.com>
2025-08-23 00:09:01 +0800

    細修

--- e-hentai.py
+++ e-hentai.py
@@ -3,49 +3,52 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import random
+page_start=9
+page_end=23
+for page in range(page_start, page_end + 1):
+    url = f"https://e-hentai.org/g/3426863/b3fb69ce86/?p={page}"
+    headers = {
+        "User-Agent": "Mozilla/5.0",
+        "Referer": "https://e-hentai.org/",
+        "Accept": "text/html",
+        "Accept-Language": "zh-TW"
+    }
 
-url = "https://e-hentai.org/g/3426863/b3fb69ce86/"
-headers = {
-    "User-Agent": "Mozilla/5.0",
-    "Referer": "https://e-hentai.org/",
-    "Accept": "text/html",
-    "Accept-Language": "zh-TW"
-}
-
-response = requests.get(url, headers=headers)
-soup = BeautifulSoup(response.text, "html.parser")
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, "html.parser")
 
 
-save_folder = soup.find('head').find('title').get_text()
-path=os.path.join('e-hentai-images',save_folder)
-os.makedirs(path, exist_ok=True)
+    save_folder = soup.find('head').find('title').get_text()
+    path=os.path.join('e-hentai-images',save_folder)
+    os.makedirs(path, exist_ok=True)
 
-a_href=soup.find('div',id="gdt").find_all('a')
+    a_href=soup.find('div',id="gdt").find_all('a')
 
-link_set=[]
-for link in a_href:
-    img_url = link.get('href')
-    time.sleep(random.uniform(1, 3))
-    response2 = requests.get(img_url, headers=headers)
-    soup2 = BeautifulSoup(response2.text, "html.parser")
-    src=soup2.find('img', id='img')
-    if src:
-        img_url2 = src.get('src')
-        link_set.append(img_url2)
-        try:
-            time.sleep(random.uniform(1, 3))
-            image_response = requests.get(img_url2, headers=headers)
-            if image_response.status_code == 200:
-                    filename = os.path.basename(img_url2)
-                    filepath = os.path.join(path, filename)
+    link_set=[]
+    for link in a_href:
+        img_url = link.get('href')
+        time.sleep(random.uniform(1, 3))
+        response2 = requests.get(img_url, headers=headers)
+        soup2 = BeautifulSoup(response2.text, "html.parser")
+        src=soup2.find('img', id='img')
+        if src:
+            img_url2 = src.get('src')
+            link_set.append(img_url2)
+            try:
+                time.sleep(random.uniform(1, 3))
+                image_response = requests.get(img_url2, headers=headers)
+                if image_response.status_code == 200:
+                        filename = os.path.basename(img_url)
+                        filepath = os.path.join(path, filename)
 
-                    with open(filepath, "wb") as f:
-                        f.write(image_response.content)
-                    print(f"已儲存：{filename}")
-            else:
-                print(f"圖片下載失敗：{img_url2}")                          
-        except Exception as e:
-                print(f"錯誤：{e}")
-    else:
-        print(f'{img_url}沒找到原圖:error')
+                        with open(filepath, "wb") as f:
+                            f.write(image_response.content)
+                        print(f"已儲存：{filename}")
+                else:
+                    print(f"圖片下載失敗：{img_url2}")                          
+            except Exception as e:
+                    print(f"錯誤：{e}")
+        else:
+            print(f'{img_url}沒找到原圖:error')
+    print(f"第 {page} 頁已完成")
 print(f"\n✅ 共抓到 {len(link_set)} 張原圖")
--- proxies_tool.py
+++ proxies_tool.py
@@ -3,13 +3,12 @@ from bs4 import BeautifulSoup
 import random
 headers = {
     "User-Agent": "Mozilla/5.0",
-    "Referer": "https://e-hentai.org/",
     "Accept": "text/html",
     "Accept-Language": "zh-TW"
     }
-def get_free_proxies(https_only: bool = True) -> list[str]:
-    url = "https://free-proxy-list.net/"
-    response = requests.get(url, headers=headers,timeout=10)
+def get_free_proxies(https_only: bool =True) -> list[str]:
+    url = "https://free-proxy-list.net/zh-tw/ssl-proxy.html"
+    response = requests.get(url, headers=headers, timeout=10)
     soup = BeautifulSoup(response.text, "html.parser")
     proxies = []
     #class有可能要改
@@ -26,13 +25,17 @@ def get_free_proxies(https_only: bool = True) -> list[str]:
         elif not https_only:
             proxies.append(f"http://{ip}:{port}")
     return proxies
-def test_proxy(proxy: str, test_url: str = "https://httpbin.org/ip", timeout: int = 5) -> bool:
+def test_proxy(proxy: str, test_url: str = "https://httpbin.org/ip") -> bool:
     try:
-        response = requests.get(test_url, proxies={"http": proxy, "https": proxy}, headers=headers ,timeout=timeout)
+        if proxy.startswith("https"):
+            proxies={"https": proxy}
+        else:
+            proxies={"http": proxy}
+        response = requests.get(test_url, proxies=proxies, headers=headers, timeout=5)
         return response.status_code == 200
     except requests.RequestException:
         return False
-def get_working_proxy(max_test: int = 30) -> str | None:
+def get_working_proxy(max_test: int = 100) -> str | None:
     proxies = get_free_proxies()
     print(f"抓到 {len(proxies)} 個代理")
     random.shuffle(proxies)
@@ -42,7 +45,9 @@ def get_working_proxy(max_test: int = 30) -> str | None:
             break
         tested += 1
         if test_proxy(proxy):
-            return proxy                                           
+            return proxy 
+        else:
+            print(f"第{tested}個代理不可用：{proxy}")                                          
     return None 
 def main():
     print(get_working_proxy())
